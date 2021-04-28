6d8a44776a2c07d32b6dbf9c755e7b9a2c7161f5
455am <83369483+455am@users.noreply.github.com>
2021-04-29 00:26:37 +0530

    Add files via upload

---
+++ smriti.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+toilet -f mono12 -F gay Smriti | pv -qL 500
+echo -e "\e[31m This is my first code\e[0m" | pv -qL 8
+echo "   "
+echo "========================================================="
+echo -e "\e[5m Thank you for trying this little software\e[0m :) "
+echo "========================================================="
